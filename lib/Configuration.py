b5dfe46483d2bda39586bc13083aca0f04ca026a
Fabio Viola <desmovalvo@gmail.com>
2016-11-16 12:36:07 +0100

    Rural simulator initialized

---
+++ lib/Configuration.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+# system-wide requirements
+import ConfigParser
+
+class Configuration:
+
+    # constructor
+    def __init__(self, config_file):
+
+        """Constructor for the Configuration class"""
+
+        # instance of the ConfigParser
+        self.cp = ConfigParser.ConfigParser()
+        self.cp.readfp(open(config_file))
+    
+
+    # logger config reader
+    def read_logger_config(self):
+        
+        """Reads the configuration of the logger"""
+        conf = {}
+        conf["log_level"] = self.cp.getint("Logger", "level")
+        return conf
+
+
+    # EVSE config reader
+    def read_evse_config(self):
+        
+        """Reads the configuration of an EVSE"""
+
+        # EVSE configuration
+        conf = {}
+        conf["evse_number"] = self.cp.getint("EVSE", "evse_number")
+        conf["storage_capacity"] = self.cp.getint("EVSE", "storage_capacity")
+        conf["storage_initial_charge"] = self.cp.getint("EVSE", "storage_initial_charge")
+        conf["storage_policy"] = self.cp.get("EVSE", "storage_policy")
+        conf["network_power"] = self.cp.getint("EVSE", "network_power")
+        conf["storage_power"] = self.cp.getint("EVSE", "storage_power")
+        return conf
+
+
---
+++ rural_simulator.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# system-wide requirements
+import logging
+
+# local requirements
+from lib.Configuration import *
+
+
+# main
+if __name__ == "__main__":
+    
+    # read the configuration file
+    c = Configuration("settings.conf")
+    logger_conf = c.read_logger_config()
+    evse_conf = c.read_evse_config()
+
+    # create an instance of the logger
+    logger = logging.getLogger('ruralSIM')
+    logger.setLevel(logger_conf["log_level"])
+    ch = logging.StreamHandler()
+    ch.setLevel(logger_conf["log_level"])
+    formatter = logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    logger.debug("Simulator configuration completed!")
+
+    # create the EVSEs
+    evses = []
+    for evse in xrange(evse_conf["evse_number"]):
+
+        # create an instance of the EVSE class for each evse
+        logger.debug("Creating EVSE %s" % len(evses))
+        evses.append("")
+        pass
